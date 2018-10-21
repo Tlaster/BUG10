99cc62ff6aa4e8dc8e62d2e75b03f76dea3bb20b
Tlaster <tlaster@outlook.com>
2018-10-21 17:44:18 +0800

    Add appveyor

---
+++ Pack.ps1
@@ -0,0 +1,4 @@
+nuget pack .\Card\Card.nuspec -Version $env:appveyor_build_version
+nuget pack .\Icon\Icon.nuspec -Version $env:appveyor_build_version
+nuget pack .\Paging\Paging.nuspec -Version $env:appveyor_build_version
+nuget pack .\Repeater\Repeater.nuspec -Version $env:appveyor_build_version
