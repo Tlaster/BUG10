99be49e1f77d7f258daedb4cf7474ba0c76fb41b
Tlaster <tlaster@outlook.com>
2018-11-14 15:55:05 +0800

    Add nuspec for Book

--- Pack.ps1
+++ Pack.ps1
@@ -2,3 +2,4 @@ nuget pack .\Card\Card.nuspec -Version $env:appveyor_build_version
 nuget pack .\Icon\Icon.nuspec -Version $env:appveyor_build_version
 nuget pack .\Paging\Paging.nuspec -Version $env:appveyor_build_version
 nuget pack .\Repeater\Repeater.nuspec -Version $env:appveyor_build_version
+nuget pack .\Book\Book.nuspec -Version $env:appveyor_build_version
